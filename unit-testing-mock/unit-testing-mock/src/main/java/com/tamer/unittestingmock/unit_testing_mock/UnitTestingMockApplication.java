0e4baa337e1554d8aaeb01116f27424a9792ad07
mohamedtamer0 <mt4452242@gmail.com>
2024-07-07 01:23:06 +0300

    update

---
+++ unit-testing-mock/unit-testing-mock/src/main/java/com/tamer/unittestingmock/unit_testing_mock/UnitTestingMockApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.unittestingmock.unit_testing_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingMockApplication.class, args);
+	}
+
+}
---
+++ unit-testing-mock/unit-testing-mock/src/test/java/com/tamer/unittestingmock/unit_testing_mock/UnitTestingMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.unittestingmock.unit_testing_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
